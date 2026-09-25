45a64b82e64e1cfb3264e7ba3f07a315f9cbe909
KiralyBence12 <118981185+KiralyBence12@users.noreply.github.com>
2026-09-25 09:36:07 +0200

    f

---
+++ 02-props/src/components/MainNavigation.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="/">Vue App</a></li>
+            <li><a href="https://vuejs.org/">Vue.js</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        background-color: lightgray;
+        padding: 1rem;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    li {
+        margin-right: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+        font-size: 1.2rem;
+        padding: 0.5rem 1rem;
+    }
+</style>
